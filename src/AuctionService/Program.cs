ce1aec0d39746c1c118a08f64d9b18e4c60a64e2
mikhita <mikheilispiriani@yahoo.com>
2024-04-07 16:56:18 +0400

    first commit

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,23 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
