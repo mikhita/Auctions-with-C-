2111674d8102c330af3766e397b9f9fc16e30ff9
mikhita <mikheilispiriani@yahoo.com>
2024-04-09 19:04:05 +0400

    adding mongodb server config and search filters

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,28 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
