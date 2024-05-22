5ca79c1b721da2bcb355fd54561eaaccf6c959c2
mikhita <mikheilispiriani@yahoo.com>
2024-05-23 01:54:14 +0400

    for check

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99094cc079c0735d62ebadf70378694542d9b7ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a857e3beccd675fb8e2ee8ece5868f8eaaeac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
